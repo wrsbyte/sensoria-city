81dd36b9e720bbc0d2b018170dcb25ca679d746b
wrsbyte <estwilmerrodriguez@gmail.com>
2025-05-07 18:52:55 -0500

    🎨 (bootcamp) Update profile and dashboard styles to replace shadows with borders for a cleaner design

--- projects/bootcamp/resources/views/livewire/pages/profile/profile.blade.php
+++ projects/bootcamp/resources/views/livewire/pages/profile/profile.blade.php
@@ -14,19 +14,19 @@ layout('layouts.app');
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white border border-gray-200">
                 <div class="max-w-xl">
                     <livewire:components.profile.update-profile-information-form />
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white border border-gray-200">
                 <div class="max-w-xl">
                     <livewire:components.profile.update-password-form />
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white border border-gray-200">
                 <div class="max-w-xl">
                     <livewire:components.profile.delete-user-form />
                 </div>
