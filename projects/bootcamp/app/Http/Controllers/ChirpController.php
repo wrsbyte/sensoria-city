e9a3a810fcd533c2a080fe064c3ba1e69adf7c71
wrsbyte <est.wilmerrodriguez@gmail.com>
2025-05-06 20:18:19 -0500

    ✨ (bootcamp) Implement Chirp functionality with event broadcasting, notifications, and CRUD operations

---
+++ projects/bootcamp/app/Http/Controllers/ChirpController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('chirps', []);
+    }
+}
--- projects/bootcamp/routes/web.php
+++ projects/bootcamp/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChirpController;
+
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
@@ -11,5 +13,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('chirps', [ChirpController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('chirps');
 
 require __DIR__.'/auth.php';
