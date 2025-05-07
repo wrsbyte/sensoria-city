a90ba67120b490818a5f9ad63b749c7da53b4cff
wrsbyte <estwilmerrodriguez@gmail.com>
2025-05-07 18:52:15 -0500

    🎨 (bootcamp) Update layout styles for app and guest views to enhance visual consistency

--- projects/bootcamp/resources/views/layouts/app.blade.php
+++ projects/bootcamp/resources/views/layouts/app.blade.php
@@ -18,12 +18,12 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            <livewire:layout.navigation />
+        <div class="min-h-screen bg-gray-100/30">
+            <x-header />
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-white border-b border-gray-200">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
