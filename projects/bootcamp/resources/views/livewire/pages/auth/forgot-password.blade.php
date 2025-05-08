a6e7f32a907ecd4494d3145a2c690eada122e011
wrsbyte <estwilmerrodriguez@gmail.com>
2025-05-07 22:06:47 -0500

    🦺 (bootcamp) example of change

--- projects/bootcamp/resources/views/livewire/pages/auth/forgot-password.blade.php
+++ projects/bootcamp/resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -51,7 +51,6 @@ $sendPasswordResetLink = function () {
             <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
                 {{ __('Email Password Reset Link') }}
