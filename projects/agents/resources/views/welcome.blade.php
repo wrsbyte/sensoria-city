0ca34250e80c5803fc9f6b55f7e7f20e6e73e642
wrsbyte <estwilmerrodriguez@gmail.com>
2025-05-08 17:21:47 -0500

    ✨ (agents) Add Filament dependency

--- projects/agents/resources/views/welcome.blade.php
+++ projects/agents/resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
+            @if (Route::has('filament.admin.auth.login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
@@ -32,7 +32,7 @@
                         </a>
                     @else
                         <a
-                            href="{{ route('login') }}"
+                            href="{{ route('filament.admin.auth.login') }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
                         >
                             Log in
