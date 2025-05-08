cb4f71dc3a9efbecadda77b568272dc8a7e7673c
wrsbyte <estwilmerrodriguez@gmail.com>
2025-05-07 20:48:13 -0500

    ✨ (bootcamp) Revamp home page layout; add SVG background and enhance content structure for better user experience

--- projects/bootcamp/resources/views/livewire/pages/home/home.blade.php
+++ projects/bootcamp/resources/views/livewire/pages/home/home.blade.php
@@ -9,6 +9,192 @@ layout('layouts.home');
 <main>
     <x-header />
     <x-container>
-        <span class="font-semibold text-red-500">wilmer</span>
+        <section class="h-full">
+            <div class="absolute right-0 top-0 -z-10 block h-full w-full overflow-hidden lg:block">
+                <svg class="absolute right-[-2px] top-[-2px] h-[calc(100%+4px)] w-auto" width="722" height="812"
+                    viewBox="0 0 722 812" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="181" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700"></rect>
+                    <rect x="271" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="91" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="1" y="1" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700"></rect>
+                    <rect x="271" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="181" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="271" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="451" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="361" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="541" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="181" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="1" y="631" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="91" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="1" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="271" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="361" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="451" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="541" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                    <rect x="631" y="721" width="90" height="90" class="stroke-gray-300 dark:stroke-gray-700">
+                    </rect>
+                </svg>
+            </div>
+            <div class="h-full py-10 min-h-[85vh]">
+                <div
+                    class="relative z-[2] col-span-10 col-start-2 row-span-8 row-start-2 flex flex-col items-start gap-10 p-15 pb-0 pt-[6rem] md:pt-[10rem] lg:pb-15 lg:pt-15">
+                    <h1 class="text-balance text-7xl md:text-8xl font-roobert w-[80%] max-w-[600px]">
+                        Sensoria City Bootcamp
+                    </h1>
+                    <div
+                        class="text-balance text-3xl font-montreal w-full max-w-[600px] text-text-secondary lg:w-[80%]">
+                        Project carried out following the guidelines provided by the Sensoria City bootcamp
+                    </div>
+
+                    <div class="h-[3.6rem] flex items-center gap-6">
+                        @if (auth()->user())
+                            <x-nav-link variant="complete" :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                                {{ __('Go to Dashboard') }}
+                            </x-nav-link>
+                        @else
+                            <x-nav-link variant="complete" :href="route('register')" :active="request()->routeIs('register')" wire:navigate>
+                                {{ __('Register') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('login')" :active="request()->routeIs('login')" wire:navigate>
+                                {{ __('Log In') }}
+                            </x-nav-link>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </section>
     </x-container>
+
+    <footer class="bg-purple-100 border-t border-gray-300 py-6 flex items-center justify-center gap-6 bottom-0 sticky">
+        <a title="wrsbyte Github" href="https://github.com/wrsbyte" target="_blank"
+            class="stroke-gray-900 hover:scale-105 transition-transform duration-200 w-7 h-7">
+            <svg stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                color="#000000">
+                <path
+                    d="M16 22.0268V19.1568C16.0375 18.68 15.9731 18.2006 15.811 17.7506C15.6489 17.3006 15.3929 16.8902 15.06 16.5468C18.2 16.1968 21.5 15.0068 21.5 9.54679C21.4997 8.15062 20.9627 6.80799 20 5.79679C20.4558 4.5753 20.4236 3.22514 19.91 2.02679C19.91 2.02679 18.73 1.67679 16 3.50679C13.708 2.88561 11.292 2.88561 8.99999 3.50679C6.26999 1.67679 5.08999 2.02679 5.08999 2.02679C4.57636 3.22514 4.54413 4.5753 4.99999 5.79679C4.03011 6.81549 3.49251 8.17026 3.49999 9.57679C3.49999 14.9968 6.79998 16.1868 9.93998 16.5768C9.61098 16.9168 9.35725 17.3222 9.19529 17.7667C9.03334 18.2112 8.96679 18.6849 8.99999 19.1568V22.0268"
+                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                </path>
+                <path d="M9 20.0267C6 20.9999 3.5 20.0267 2 17.0267" stroke-width="1.5" stroke-linecap="round"
+                    stroke-linejoin="round">
+                </path>
+            </svg>
+        </a>
+        <a title="wrsbyte LinkedIn" href="https://www.linkedin.com/in/wrsbyte" target="_blank"
+            class="stroke-gray-900 hover:scale-105 transition-transform duration-200 w-7 h-7">
+            <svg stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M21 8V16C21 18.7614 18.7614 21 16 21H8C5.23858 21 3 18.7614 3 16V8C3 5.23858 5.23858 3 8 3H16C18.7614 3 21 5.23858 21 8Z"
+                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M7 17V13.5V10" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M11 17V13.75M11 10V13.75M11 13.75C11 10 17 10 17 13.75V17" stroke-width="1.5"
+                    stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M7 7.01L7.01 6.99889" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+                </path>
+            </svg>
+        </a>
+    </footer>
 </main>
